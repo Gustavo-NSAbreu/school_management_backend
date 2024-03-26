ecdfbad5afc4d53651980970f2daa7b5dc60614c
Gustavo Abreu <guustavoabreu99@gmail.com>
2024-03-26 20:56:40 -0300

    First commit

---
+++ src/main/java/com/web_development_class/school_management_backend/SchoolManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.web_development_class.school_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web_development_class/school_management_backend/SchoolManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_development_class.school_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
